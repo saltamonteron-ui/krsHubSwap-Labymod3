ba9850f8080d0bf258db3051bb70fd87f38bb2c3
saltamonteron-ui <saltamonteron@gmail.com>
2026-07-08 02:11:06 +0300

    Add KrsHubSwap.java

---
+++ src/main/java/ru/krosovok/krshubswap/KrsHubSwap.java
@@ -0,0 +1,22 @@
+package ru.krosovok.krshubswap;
+
+import net.labymod.api.addon.LabyAddon;
+import net.labymod.api.models.addon.AddonConfig;
+
+public class KrsHubSwap extends LabyAddon {
+
+    @Override
+    public AddonConfig getConfig() {
+        return null;
+    }
+
+    @Override
+    public void load() {
+
+    }
+
+    @Override
+    public void unload() {
+
+    }
+}
